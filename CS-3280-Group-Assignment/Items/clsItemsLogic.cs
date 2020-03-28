71bbdbfa0205ebb17a3e5957dfa49eb41ec57483
romelaa <azizyanromela75@gmail.com>
2020-03-27 22:01:06 -0600

    added sql statements for Items

--- CS-3280-Group-Assignment/Items/clsItemsLogic.cs
+++ CS-3280-Group-Assignment/Items/clsItemsLogic.cs
@@ -8,5 +8,32 @@ namespace CS_3280_Group_Assignment.Items
 {
     class clsItemsLogic
     {
+        /// <summary>
+        /// Method to add an item.
+        /// </summary>
+        public static void addItem() { 
+            
+        }
+
+        /// <summary>
+        /// Method to delete an item.
+        /// </summary>
+        public static void deleteItem() { 
+        
+        }
+
+        /// <summary>
+        /// Method to edit an item.
+        /// </summary>
+        public static void editItem() { 
+        
+        }
+
+        /// <summary>
+        /// Method to get the invoices that the selected item is in.
+        /// </summary>
+        public static void getInvoicesWithItem() { 
+        
+        }
     }
 }
--- CS-3280-Group-Assignment/Items/clsItemsSQL.cs
+++ CS-3280-Group-Assignment/Items/clsItemsSQL.cs
@@ -8,5 +8,53 @@ namespace CS_3280_Group_Assignment.Items
 {
     class clsItemsSQL
     {
+        /// <summary>
+        /// Query to add an item to the Items Table.
+        /// </summary>
+        /// <param name="itemName"></param>
+        /// <param name="cost"></param>
+        /// <returns></returns>
+        public static string AddItem(string itemName, double cost)
+        {
+            try
+            {
+                return "INSERT INTO Items(ItemName, ItemCost) VALUES('" + itemName + "', " + cost + ")";
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Unable to add item." + ex.ToString());
+            }
+        }
+
+        /// <summary>
+        /// Query to delete an item from the Items Table.
+        /// </summary>
+        /// <param name="itemID"></param>
+        /// <returns></returns>
+        public static string DeleteItem(int itemID) {
+            try
+            {
+                return "DELETE FROM Items WHERE ItemID = " + itemID;
+            }
+            catch (Exception ex) {
+                throw new Exception("Unable to delete item." + ex.ToString());
+            }
+        }
+
+        /// <summary>
+        /// Query to edit an item from the Items Table.
+        /// </summary>
+        /// <param name="itemID"></param>
+        /// <param name="itemName"></param>
+        /// <param name="cost"></param>
+        /// <returns></returns>
+        public static string EditItem(int itemID, string itemName, double cost) {
+            try {
+                return "UPDATE Items SET ItemName = '" + itemName + "', ItemCost = " + cost + " WHERE" +
+                    "ItemmID = " + itemID;
+            } catch (Exception ex) {
+                throw new Exception("Unable to edit item." + ex.ToString()); 
+            }
+        }
     }
 }
